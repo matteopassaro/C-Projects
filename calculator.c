0f637d61584ebb3219a78705d8150e4c2883798e
Matteo Passaro <48924404+matteopassaro@users.noreply.github.com>
2023-02-10 14:15:55 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,39 +1,44 @@
 /*
   This is a pretty simple code for a basic calulator in C.
-  It can only do operations between two operands.
   The only operations allowed are the ones of a basic calculator (+, -, *, /, ^).
   I will upgrade this program whenever I'll find a better way to do it.
-  The one thing I want to do as soon as possible is to find a way to do operations between multiple operands.
 */
 
 #include <stdio.h>
 #include <math.h>
 
 int main(){
-    double calculate(double firstOp, double secondOp, char operationChar);
-    double op1, op2;
-    double result;
+    double calculate(double firstOp, double secondOp, char operationChar); // Inizialize the function to calculate the operation
+    double op1, op2;                                                      //  op1 and op2 are the two operands
+    double result, firstResult;
     char operation;
 
 
     printf("Enter the first operand: ");
     scanf("%lf", &op1);
+    
+    while(1){
+        printf("\nChoose the operation (+, -, *, /, ^) or 'q' to quit.\n");
+        printf("--> ");
+        scanf(" %c", &operation);
+        
+        if(operation == 'q' || operation == 'Q'){               // if the user inputs charchter 'q' or 'Q' the program ends
+            break;
+        }
+        
+        printf("Enter the next operand: ");
+        scanf("%lf", &op2);
 
-    printf("Enter the second operand: ");
-    scanf("%lf", &op2);
-
-    printf("\nChoose the operation (+, -, *, /, ^)\n");
-    printf("--> ");
-    scanf(" %c", &operation);
-    puts("");
-
-    result = calculate(op1, op2, operation);    
-    printf("%.2lf %c %.2lf = %.2lf", op1, operation, op2, result);
-
+        puts("");  //it's just here to make a space between the lines
+        result = calculate(firstResult, op2, operation);        // Here we send the operands and the operator to the function
+        printf("%.2lf %c %.2lf = %.2lf", firstResult, operation, op2, result);
+        op1 = result;         // Update the op1 so next time it will show the latest result operated with the number
+        puts("");
+    }
 }
 
-double calculate(double firstOp, double secondOp, char operationChar){
-    switch(operationChar){
+double calculate(double firstOp, double secondOp, char operationChar){          // The function just uses a switch case to 
+    switch(operationChar){                                                      //  get the character value and returns the result
         case '+':
             return firstOp+secondOp;
             break;
